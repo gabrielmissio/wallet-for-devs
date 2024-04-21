9ed0ffaf58a90e663698347663c12646618f52c7
gabrielmissio <gabrielmissio.dev@gmail.com>
2024-04-21 14:35:30 -0300

    feat: Add BruteForceAttack PoC

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-const qrcode = require('qrcode-terminal')
+const { generateMnemonic } = require('./infra/helpers/key-helper')
 
-require('dotenv').config()
-const BTCSegWitSignTransactionStrategy = require('./domain/strategies/sign-transaction/btc-segwit-strategy')
-const SignTransactionUseCase = require('./domain/use-cases/sign-transaction-use-case')
-const BTCKeyRepository = require('./infra/repositories/btc-key-repository')
-
-const keyName = 'any-key-alias'
-const keyToMnemonic = new Map()
-keyToMnemonic.set(keyName, process.env.DEVELOPMENT_MNEMONIC)
-
-const btcSegWitTestnetBasePath = "m/84'/0'/0'"
-
-const btcSegWitSignStrategy = new BTCSegWitSignTransactionStrategy({
-  keyRepository: new BTCKeyRepository({ keyToMnemonic })
-})
-const signTransactionUseCase = new SignTransactionUseCase({
-  protocolStrategy: btcSegWitSignStrategy
-})
-
-signTransactionUseCase.signTransaction({
-  keyName, path: btcSegWitTestnetBasePath, payload: process.env.RAW_TX
-}).then((data) => {
-  qrcode.generate(data.qrCodeData, { small: true })
-  console.log(data.qrCodeData)
-})
+console.log({ mnemonic: generateMnemonic() })
---
+++ src/pocs/brute-force-attack.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+require('dotenv').config()
+
+const { generateMnemonic } = require('../infra/helpers/key-helper')
+const AccountDiscoveryUseCase = require('../domain/use-cases/account-discovery-use-case')
+const BTCBlockchainAPI = require('../infra/apis/btc-blockchain-api')
+const HttpHelper = require('../infra/helpers/http-helper')
+const BTCKeyRepository = require('../infra/repositories/btc-key-repository')
+
+const keyToMnemonic = new Map()
+const btcSegWitTestnetBasePath = "m/84'/0'/0'/0"
+
+const btcTestnetHttpClient = new HttpHelper({
+  baseURL: process.env.BTC_TESTNET_BLOCKCHAIN_API_URL,
+  globalTimeout: 10000
+})
+const btcTestnetBlockchainApi = new BTCBlockchainAPI({ httpClient: btcTestnetHttpClient })
+const btcKeyRepositoryTestnet = new BTCKeyRepository({ keyToMnemonic }) // default network is testnet
+const accountDiscoveryUseCase = new AccountDiscoveryUseCase({
+  blockchainAPI: btcTestnetBlockchainApi,
+  keyRepository: btcKeyRepositoryTestnet,
+  gapLimit: 1
+})
+
+async function main (bruteForceAttemptsLimit) {
+  let attempts = 0
+  let isEmptyAccount = true
+  const keyName = 'brute-force-attack'
+
+  while (isEmptyAccount && attempts < bruteForceAttemptsLimit) {
+    const mnemonic = generateMnemonic() // process.env.DEVELOPMENT_MNEMONIC
+    keyToMnemonic.set(keyName, mnemonic)
+    console.log({ attempt: attempts, mnemonic })
+
+    isEmptyAccount = await accountDiscoveryUseCase.checkAccountActivity({
+      keyName,
+      path: btcSegWitTestnetBasePath
+    })
+
+    if (!isEmptyAccount) {
+      const foundAt = new Date().toISOString()
+      fs.mkdirSync('../brute-force-attack', { recursive: true })
+      fs.writeFileSync(`../brute-force-attack/${foundAt}.txt`, mnemonic)
+      console.log(`Mnemonic saved into ../brute-force-attack/${foundAt}.txt`)
+    }
+
+    attempts++
+  }
+
+  console.log('\nBrute force attack finished!')
+  if (isEmptyAccount) {
+    console.log('No luck this time!')
+  } else {
+    console.log('You are the chosen one!')
+  }
+  console.log(`Total attempts: ${attempts}`)
+}
+
+// You can increase this limit whatever you want, it's almost impossible to find a non-empty account
+const bruteForceAttemptsLimit = 50
+main(bruteForceAttemptsLimit)
